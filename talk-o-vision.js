c550cf59d7f22797bccc5221d5ffb3e49e1c6f8e
Scott Davis <sdavis@thoughtworks.com>
2017-03-23 15:26:37 -0600

    added page breaks, 'f' for fullscreen

--- talk-o-vision.js
+++ talk-o-vision.js
@@ -3,10 +3,17 @@
 window.addEventListener('load', slideshowInit);
 window.addEventListener('keydown', keyHandler);
 
+/**
+  * Slideshow initialization
+  */
 function slideshowInit(){
   numberSlides();
 }
 
+/**
+  * Adds an "id" attribute to each slide containing the slide number.
+  * This enables hash / fragment navigation (http://foo.com/#12).
+  */
 function numberSlides(){
   let slides = document.querySelectorAll(".slideshow li");
   window.sessionStorage.slideCount = slides.length;
@@ -17,6 +24,10 @@ function numberSlides(){
   }
 }
 
+/**
+  * Enables keyboard shortcuts
+  * For example: next, previous, fullscreen
+  */
 function keyHandler(e){
   switch(e.which){
     case 37: // left
@@ -24,12 +35,20 @@ function keyHandler(e){
       previousSlide()
       break;
     case 39: // right
+    case 32: // spacebar
       e.preventDefault();
       nextSlide()
+      break;
+    case 70: // f
+      e.preventDefault();
+      fullscreen();
       break;
   }
 }
 
+/**
+  * Advances slideshow to next slide
+  */
 function nextSlide(){
   let current = window.location.hash.replace("#", "") * 1;
   let next = current + 1;
@@ -39,6 +58,9 @@ function nextSlide(){
   window.location.hash = next;
 }
 
+/**
+  * Moves slideshow to previous slide
+  */
 function previousSlide(){
   let current = window.location.hash.replace("#", "") * 1;
   let previous = current - 1;
@@ -46,4 +68,20 @@ function previousSlide(){
     previous = 1;
   }
   window.location.hash = previous;
+}
+
+/**
+  * Enables fullscreen
+  */
+function fullscreen(){
+  let html = document.querySelector("html");
+  let requestFullscreen = html.requestFullscreen ||
+                          html.requestFullScreen ||
+                          html.webkitRequestFullscreen ||
+                          html.mozRequestFullScreen ||
+                          html.msRequestFullscreen;
+
+  if (requestFullscreen) {
+    requestFullscreen.apply(html);
+  }
 }
