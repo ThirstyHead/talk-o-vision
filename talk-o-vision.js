e33f38d9835841f0608a4be896a324a9c52200f5
Scott Davis <sdavis@thoughtworks.com>
2017-03-23 14:47:03 -0600

    added keyhandler for next, previous

--- talk-o-vision.js
+++ talk-o-vision.js
@@ -1,12 +1,49 @@
 'use strict';
 
-window.onload = slideshowInit();
+window.addEventListener('load', slideshowInit);
+window.addEventListener('keydown', keyHandler);
 
 function slideshowInit(){
+  numberSlides();
+}
+
+function numberSlides(){
   let slides = document.querySelectorAll(".slideshow li");
+  window.sessionStorage.slideCount = slides.length;
 
   //number slides for #x url fragment navigation
   for(let i=0; i<slides.length; i++){
     slides[i].id = `${i + 1}`;
   }
+}
+
+function keyHandler(e){
+  switch(e.which){
+    case 37: // left
+      e.preventDefault();
+      previousSlide()
+      break;
+    case 39: // right
+      e.preventDefault();
+      nextSlide()
+      break;
+  }
+}
+
+function nextSlide(){
+  let current = window.location.hash.replace("#", "") * 1;
+  let next = current + 1;
+  if(next >= window.sessionStorage.slideCount){
+    next = window.sessionStorage.slideCount;
+  }
+  window.location.hash = next;
+}
+
+function previousSlide(){
+  let current = window.location.hash.replace("#", "") * 1;
+  let previous = current - 1;
+  if(previous <= 1){
+    previous = 1;
+  }
+  window.location.hash = previous;
 }
