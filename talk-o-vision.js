b9cff634df4c59c55203219b4f31ca49b3a3a769
Scott Davis <scott@thirstyhead.com>
2018-01-18 10:23:31 -0700

    refactored functionality into Slides class

--- talk-o-vision.js
+++ talk-o-vision.js
@@ -6,113 +6,15 @@ window.addEventListener('load', slideshowInit);
   * Slideshow initialization
   */
 function slideshowInit(){
-  numberSlides();
-  window.addEventListener('keydown', keyHandler);
-}
-
-/**
-  * Adds an "id" attribute to each slide containing the slide number.
-  * This enables hash / fragment navigation (e.g. http://foo.com/#12).
-  */
-function numberSlides(){
-  let s = new Slides();
-  let slideList = s.all;
-  for(let i=0; i<slideList.length; i++){
-    slideList[i].id = `${i + 1}`;
-  }
-}
-
-/**
-  * Enables keyboard shortcuts
-  * For example: next, previous, fullscreen
-  */
-function keyHandler(e){
-  switch(e.which){
-    case 33: // pgup
-    case 37: // left
-      e.preventDefault();
-      previousSlide()
-      break;
-
-    case 32: // spacebar
-    case 34: // pgdn
-    case 39: // right
-      e.preventDefault();
-      nextSlide()
-      break;
-
-    case 70: // f
-      e.preventDefault();
-      fullscreen();
-      break;
-
-    case 78: // n
-      //toggle showNotes
-      // let showNotes = ( window.localStorage.getItem('showNotes') === 'true' );
-      // window.localStorage.setItem('showNotes', !showNotes);
-      break;
-  }
-}
-
-
-/**
-  * returns the current slide number
-  */
-function currentSlideNumber(){
-  let currentSlideNumber = window.location.hash.replace("#", "") * 1 || 1;
-  return currentSlideNumber;
-}
-
-
-
-
-
-
-/**
-  * Advances slideshow to next slide
-  */
-function nextSlide(){
-  let current = currentSlideNumber();
-  let next = current + 1;
-  let slideCount = new Slides().list.length;
-  if(next >= slideCount){
-    next = slideCount;
-  }
-  window.location.hash = next;
-}
-
-/**
-  * Moves slideshow to previous slide
-  */
-function previousSlide(){
-  let current = currentSlideNumber();
-  let previous = current - 1;
-  if(previous <= 1){
-    previous = 1;
-  }
-  window.location.hash = previous;
-}
-
-/**
-  * Enables fullscreen
-  */
-function fullscreen(){
-  let html = document.querySelector("html");
-  let requestFullscreen = html.requestFullscreen ||
-                          html.requestFullScreen ||
-                          html.webkitRequestFullscreen ||
-                          html.mozRequestFullScreen ||
-                          html.msRequestFullscreen;
-
-  if (requestFullscreen) {
-    requestFullscreen.apply(html);
-  }
+  let slides = new Slides();
 }
 
 
 class Slides{
   constructor(){
     this.list = this.all;
+    this.numberSlides();
+    window.addEventListener('keydown', evt => this.keyHandler(evt));
   }
 
   /**
@@ -121,5 +23,94 @@ class Slides{
   get all(){
     let all = document.querySelectorAll(".slideshow > section");
     return all;
+  }
+
+  /**
+    * returns the current slide number
+    */
+  get current(){
+    let currentSlideNumber = window.location.hash.replace("#", "") * 1 || 1;
+    return currentSlideNumber;
+  }
+
+  /**
+    * Adds an "id" attribute to each slide containing the slide number.
+    * This enables hash / fragment navigation (e.g. http://foo.com/#12).
+    */
+  numberSlides(){
+    for(let i=0; i<this.all.length; i++){
+      this.all[i].id = `${i + 1}`;
+    }
+  }
+
+  /**
+    * Moves slideshow to previous slide
+    */
+  previous(){
+    let previous = this.current - 1;
+    if(previous <= 1){
+      previous = 1;
+    }
+    window.location.hash = previous;
+  }
+
+  /**
+    * Advances slideshow to next slide
+    */
+  next(){
+    let next = this.current + 1;
+    let slideCount = this.all.length;
+    if(next >= slideCount){
+      next = slideCount;
+    }
+    window.location.hash = next;
+  }
+
+  /**
+    * Enables fullscreen
+    */
+  fullscreen(){
+    let html = document.querySelector("html");
+    let requestFullscreen = html.requestFullscreen ||
+                            html.requestFullScreen ||
+                            html.webkitRequestFullscreen ||
+                            html.mozRequestFullScreen ||
+                            html.msRequestFullscreen;
+
+    if (requestFullscreen) {
+      requestFullscreen.apply(html);
+    }
+  }
+
+  /**
+    * Enables keyboard shortcuts
+    * For example: next, previous, fullscreen
+    */
+  keyHandler(e){
+    switch(e.which){
+      case 33: // pgup
+      case 37: // left
+        e.preventDefault();
+        this.previous();
+        break;
+
+      case 32: // spacebar
+      case 34: // pgdn
+      case 39: // right
+        e.preventDefault();
+        this.next()
+        break;
+
+      case 70: // f
+        e.preventDefault();
+        this.fullscreen();
+        break;
+
+      case 78: // n
+        //toggle showNotes
+        // let showNotes = ( window.localStorage.getItem('showNotes') === 'true' );
+        // window.localStorage.setItem('showNotes', !showNotes);
+        break;
+    }
   }
 }
