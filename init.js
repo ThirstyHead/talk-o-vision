1137acaf34ff3cc4152b02769c05d257fadc053a
Scott Davis <scott@thirstyhead.com>
2019-03-06 10:41:02 -0700

    added splash screen to init.js

--- init.js
+++ init.js
@@ -4,6 +4,7 @@ cp -riv Caddyfile ../..
 cp -riv apple-touch-icon-precomposed.png ../..
 cp -riv apple-touch-icon.png ../..
 cp -riv favicon.ico ../..
+cp -riv index.html ../..
 cp -riv server.js ../..
 cp -riv slides ../..
 cp -riv utils ../..
